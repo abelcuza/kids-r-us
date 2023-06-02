4cb3becaa9e68fce0fc8a129c95138e79b1a3bb3
Abel Cuza <abel.cuza07@gmail.com>
2023-06-01 23:08:51 -0400

    Add custom fonts provided in the resources

---
+++ app/fonts.js
@@ -0,0 +1,14 @@
+import {Francois_One, Gilda_Display, Quicksand} from "next/font/google"
+
+export const francoisOne = Francois_One({
+    subsets: ['latin'],
+    weight: "400"
+})
+export const gildaDisplay = Gilda_Display({
+    subsets: ['latin'],
+    weight: "400"
+})
+
+export const quicksand = Quicksand({
+    subsets: ['latin'],
+})
