0dff5e8a6559f6371881a3ab85c0fed5db566ab6
Abel Cuza <abel.cuza07@gmail.com>
2023-06-01 23:09:24 -0400

    Add css and page component

--- app/page.js
+++ app/page.js
@@ -1,95 +1,105 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import Box from "@mui/material/Box";
+import Image from "next/image";
+import {Button} from "@mui/material";
+import {bestSeller, categories, instagram_pictures, menuCategories, products} from "@/app/data";
+import Category from "@/app/components/category";
+import SearchBar from "@/app/components/search-bar";
+import CategoriesMenu from "@/app/components/categories-menu";
+import BestSellerMenu from "@/app/components/best-seller-menu";
+import NewArrivals from "@/app/components/new-arrivals";
+import InstagramPictures from "@/app/components/instagram-pictures";
+import SuscribeNewsletter from "@/app/components/suscribe-newsletter";
+import {francoisOne, gildaDisplay, quicksand} from "@/app/fonts";
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    const categorySectionStyle = {
+        display: "flex",
+        justifyContent: "center",
+        border: "1px dashed",
+        borderColor: "lightblue",
+        padding: "40px 80px"
+    }
+    return (
+        <>
+            <section>
+                <Box sx={{display: "flex", justifyContent: "space-between", width: "100%"}}>
+                    <div style={{position: "relative"}}>
+                        <Image src="/homepage/slider-1.jpg" width={698} height={470} alt="homepage-slider-1"/>
+                        <div style={{position: "absolute", left: "5%", top: "50%", color: "white"}}>
+                            <h1 className={francoisOne.className}>JUMPSUITS</h1>
+                            <p>Confortable clothes for your little babbies</p>
+                            <Button variant="contained">Discover</Button>
+                        </div>
+                    </div>
+                    <div>
+                        <Image src="/homepage/girls-power.jpg" alt="homepage-slider-2" width={340} height={418}/>
+                        <Box sx={{textAlign: "center"}}>
+                            <h2 className={francoisOne.className}>GIRLS POWER</h2>
+                            <span className="text-muted">For a colorfull summer</span>
+                        </Box>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+                    </div>
+                </Box>
+            </section>
+            <section style={categorySectionStyle}>
+                <Box sx={{display: "flex", justifyContent: "space-between", width: "100%", margin: "0px 30px"}}>
+                    {categories.map((category) => (
+                        <Category key={category.name} {...category} />
+                    ))}
+                </Box>
+            </section>
+            <section style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+                <h2 className={francoisOne.className}>NEW ARRIVALS</h2>
+                <Image src="/hr.png" width={76} height={12}/>
+                <Box sx={{display: "flex", justifyContent: "space-between", width: "100%"}}>
+                    {/*Left Side*/}
+                    <Box sx={{
+                        display: "flex",
+                        flexDirection: "column",
+                        width: "25%",
+                        margin: "0 10px",
+                        marginTop: "30px"
+                    }}>
+                        {/*Search Bar*/}
+                        <SearchBar/>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+                        {/*Category Menu*/}
+                        <Box sx={{margin: "10px 0"}}>
+                            <h3>CATEGORIES</h3>
+                            <CategoriesMenu categories={menuCategories}/>
+                        </Box>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+                        {/*Best Seller*/}
+                        <Box sx={{margin: "10px 0"}}>
+                            <h3>BEST SELLERS</h3>
+                            <BestSellerMenu items={bestSeller}/>
+                        </Box>
+                        <Box sx={{margin: "10px 0"}}>
+                            <SuscribeNewsletter/>
+                        </Box>
+                    </Box>
+                    {/*Right Side*/}
+                    <Box sx={{width: "70%", marginTop: "30px"}}>
+                        {/*New Arrivals*/}
+                        <NewArrivals items={products.sort((a, b) => {
+                            if (a.arrival_date.getTime() > b.arrival_date.getTime()) {
+                                return -1;
+                            } else if (a.arrival_date.getTime() < b.arrival_date.getTime()) {
+                                return 1;
+                            } else {
+                                return 0;
+                            }
+                        }).slice(0, 9)}/>
+                    </Box>
+                </Box>
+            </section>
+            <section style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+                <h2 className={francoisOne.className}>INSTAGRAM @KIDSRUS</h2>
+                <Image src="/hr.png" width={76} height={12} alt={"hr"}/>
+                <InstagramPictures items={instagram_pictures}/>
+            </section>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+        </>
+    )
 }
